4f2a44ded3400461702abf4792f0dc905a4df34a
wjygamedev <wjygamedev@126.com>
2015-07-09 06:31:24 +0800

    [modify] UTC

--- taixinkanghu/settings.py
+++ taixinkanghu/settings.py
@@ -94,13 +94,13 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'PRC'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
