3f80946a36490922a0c89d5e17f3f1541aa7e4fd
wjygamedev <wjygamedev@126.com>
2015-07-09 05:44:45 +0800

    [modify]bug not found admin

--- taixinkanghu/urls.py
+++ taixinkanghu/urls.py
@@ -27,8 +27,10 @@ Including another URLconf
 # ]
 
 from django.conf.urls import patterns, url, include
+from django.contrib import admin
 from helloworld.views import *
 
 urlpatterns = [
     url(r'^$', main_page),
+    url(r'^admin/', include(admin.site.urls)),
 ]
