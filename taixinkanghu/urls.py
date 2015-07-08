e4aa88a6acf2409ae17f10c74142a1022f571d48
wjygamedev <wjygamedev@126.com>
2015-07-09 03:47:05 +0800

    [modify] test helloworld(bookmard) info.

--- taixinkanghu/urls.py
+++ taixinkanghu/urls.py
@@ -13,15 +13,21 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
+# from django.conf.urls import include, url
+# from django.contrib import admin
+#
+# urlpatterns = [
+#     url(r'^admin/', include(admin.site.urls)),
+# ]
 # from django.conf.urls.defaults import *
 # from taixinkuanghu.views import hello
 #
 # urlpatterns = [
 #     ('^hello/$', hello),
 # ]
+from django.conf.urls.defaults import *
+from helloworld.views import *
+
+urlpatterns = [
+    (r'^$', main_page),
+]
