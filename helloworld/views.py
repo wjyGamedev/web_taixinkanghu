a894f0ad989ddccf78954062fd86d1bb0b611ce1
wjygamedev <wjygamedev@126.com>
2015-08-02 06:12:36 +0800

    [modify]删除debug flag

--- helloworld/views.py
+++ helloworld/views.py
@@ -41,16 +41,13 @@ class MobSMS:
         self.verify_url = 'https://api.sms.mob.com/sms/verify'
 
     def verify_sms_code(self, zone, phone, code, debug=False):
-        if debug:
-            return 200
-
         data = {'appkey': self.appkey, 'phone': phone, 'zone': zone, 'code': code}
         req = requests.post(self.verify_url, data=data, verify=False)
         if req.status_code == 200:
             j = req.json()
             return j.get('status', 500)
 
-        return 500
+        return HttpResponse(req.status_code, content_type="text/plain")
 
 
 @csrf_exempt
