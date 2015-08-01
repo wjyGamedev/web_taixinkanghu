4053da33f6a10c62a320c20934b1de342875dd5f
wjygamedev <wjygamedev@126.com>
2015-08-02 02:34:54 +0800

    [modify]修改log格式

--- helloworld/views.py
+++ helloworld/views.py
@@ -58,7 +58,7 @@ def handle_register(requset):
     if requset.is_ajax():
         if requset.method == 'POST':
             print >>sys.stdout, 'Raw Data: "%s"' % requset.body
-            logger.debug("Raw Data:="+requset.body)
+            logger.debug('Raw Data: "%s"' % requset.body)
     return HttpResponse("OK")
 
 
